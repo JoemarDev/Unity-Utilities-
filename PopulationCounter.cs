89861f23fd72859018df3dfbeade2581cc6c8423
Halim Lais <halimlais@hotmail.com>
2018-10-16 21:49:43 +0100

    Add files via upload

---
+++ PopulationCounter.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PopulationCounter : MonoBehaviour {
+
+    [SerializeField] GameObject timer;
+    public InputField userInput;
+
+    [SerializeField]GameObject button;
+
+    float timeUpdater;
+    public float speedOfTime = 2.5f;
+    int initialvalue;
+
+    int totalTime;
+    string displayCounter;
+    
+    private void FixedUpdate()
+    {
+       
+        if (initialvalue > 1)
+        {
+           
+            timeUpdater += Time.fixedDeltaTime * speedOfTime;
+            int roundedsecs = (int)timeUpdater;
+            totalTime = initialvalue + roundedsecs;
+            displayCounter = totalTime.ToString("n");
+
+            timer.GetComponent<Text>().text = displayCounter.Remove(displayCounter.Length - 3);
+            DisableFields();
+
+        }
+    }
+
+    public void TextInputInitialValue()
+    {
+        initialvalue = int.Parse( userInput.text);
+    }
+
+    public void DisableFields()
+    {
+        button.SetActive(false);
+        userInput.gameObject.SetActive(false);
+        
+    }
+
+}
