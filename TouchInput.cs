690476a155f40a4205e0583adba5c27c04e548c5
Halim Lais <halimlais@hotmail.com>
2019-02-03 17:11:39 +0000

    Add files via upload
    simple touch input - left and right

---
+++ TouchInput.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TouchInput : MonoBehaviour {
+
+    int TouchMove()
+    {
+        int axis = 0;
+        if (Input.touchCount > 0)
+        {
+            Touch touch = Input.GetTouch(0);
+           float centrePoint= Screen.width / 2;
+            if (touch.position.x < centrePoint && touch.phase == TouchPhase.Began)
+            {
+                axis = -1;
+            }
+            if (touch.position.x > centrePoint)
+            {
+                axis = 1;
+            }
+        }
+        return axis;
+    }
+}
