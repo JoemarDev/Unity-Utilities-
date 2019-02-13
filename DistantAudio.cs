64e148bb099ad1a956c863b9297de8b6796081a6
Halim Lais <halimlais@hotmail.com>
2019-02-13 16:51:18 +0000

    Add files via upload

---
+++ DistantAudio.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DistantAudio : MonoBehaviour {
+
+    public Transform player;
+    AudioSource sound;
+    [SerializeField] float distanceOfAudio = 50;
+    // Use this for initialization
+    void Start()
+    {
+        sound = GetComponent<AudioSource>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!player) return;
+        var d = Vector3.Distance(player.position, transform.position);
+        Debug.Log(d);
+        
+       
+        sound.volume = Map(d, 0, distanceOfAudio, 1, -1);
+        Debug.Log(sound.volume);
+    }
+
+    public  float Map(float x, float in_min, float in_max, float out_min, float out_max)
+    {
+        return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
+    }
+}
