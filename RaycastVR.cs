4c4dc5f4b4a09ea3535c9a569845754eb105753b
Halim Lais <halimlais@hotmail.com>
2019-09-03 16:32:35 +0100

    Add files via upload
    VR raycast

---
+++ RaycastVR.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RaycastVR : MonoBehaviour
+{
+
+    public Camera vrHeadSet;
+    [SerializeField] float distanceOfRay = 3f;
+    Ray ray;
+    RaycastHit hitObject;
+   
+  
+    void Update()
+    {
+        ray = new Ray(transform.position, vrHeadSet.transform.forward);
+        Debug.DrawRay(  transform.localPosition, vrHeadSet.transform.forward ,Color.red);
+
+        if (Physics.Raycast(ray, out hitObject, 3f))
+        {
+            //tag objects in inspector
+            if (hitObject.collider.tag == "house")
+            {
+                Debug.Log("hi");
+            }
+        }
+    }
+}
