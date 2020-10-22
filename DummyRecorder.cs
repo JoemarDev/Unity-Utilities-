e9573c2847f8614810a19c6a7c0a27e94d48ca76
Logck <halimlais@hotmail.com>
2020-10-22 14:30:56 +0100

    Add files via upload

---
+++ DummyRecorder.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(AudioSource))]
+public class DummyRecorder : MonoBehaviour
+{
+    public Slider indicatorPeak;
+    public Slider indicatorRMS;
+    
+
+    private void Update()
+    {
+        indicatorPeak.value = MicInput.MicLoudnessPeak ;
+        indicatorRMS.value = MicInput.MicLoudnessRMS * MicSensivitiy;
+    }
+
+}
